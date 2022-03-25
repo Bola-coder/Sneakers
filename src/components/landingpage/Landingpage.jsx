0c4be9dfc2f2f2a562fd532c3a1f4a35c01ef2a2
ademola <sikirusulaimon2002@gmail.com>
2022-03-25 20:30:14 +0100

    added ade 2002 a remote history

--- src/components/landingpage/Landingpage.jsx
+++ src/components/landingpage/Landingpage.jsx
@@ -22,6 +22,7 @@ const Landingpage = () => {
             <h1 className={Style.mensText}><strong>We Might Have What You Like</strong></h1>
             <p id='lorem' /* onLoad={writeText} */></p>
         </section>
+        
     )
 }
 export default Landingpage;
