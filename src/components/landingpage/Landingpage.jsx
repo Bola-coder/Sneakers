da04bffe957a7a77c57766286d4fa2de5744a1cb
ademola <sikirusulaimon2002@gmail.com>
2022-03-26 15:27:25 +0100

    check out access to origin remote

--- src/components/landingpage/Landingpage.jsx
+++ src/components/landingpage/Landingpage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Style from './sass/style.module.scss';
 import shirt from './img/shirt.png'
 import { Link } from "react-router-dom";
@@ -6,33 +6,31 @@ const Landingpage = () => {
     let lorem = document.getElementById("lorem");
     let text = "Lorem Ipsum has been the industry's standard dummy....";
     let index = 0;
-    /*  const writeText = () => {
-         lorem.innerHTML = text.slice(0, index);
-         index++;
-         if (index > text.length - 0) {
-             index = 0;
-         }
-     } *//* 
-    setInterval(writeText, 150) */
-    /*   window.addEventListener('load', function () {
-          writeText()
-      }) */
+    const [writeText, setwriteText] = useState(false)
+
+    function writeTexts(){
+        lorem.innerHTML = text.slice(0, index);
+        index++;
+        if (index > text.length - 0) {
+            index = 0;
+        }
+    }
+    setInterval(writeTexts, 150)
+    window.addEventListener('load', function () {
+        writeTexts()
+        setwriteText(true)
+    })
+    console.log(lorem)
 
     return (
         <section className={Style.homesection}>
             <div className={Style.hometext}>
                 <h3 className={Style.bannerText}>Checkout Our New Wares.</h3>
                 <h1 className={Style.mensText}><strong>We  Might Have What You Like..</strong></h1>
-                <p id='lorem' className={`${Style.mobile}`} /* onLoad={writeText} */>Lorem Ipsum has been the industry's standard dummy....</p>
-                <span>
-                    <Link to='/'>
-                    <button>Buy Now</button>
-                    </Link>
-                    <Link to='/'>
-                        <button>
-                            See More
-                        </button>
-                    </Link>
+                <p id='lorem' className={`${Style.mobile}`}>  </p>
+                <span> 
+                    <Link to='/'><button>Buy Now</button></Link>
+                    <Link to='/'><button>See More</button></Link>
                 </span>
             </div>
             <img alt='shirt' src={shirt} />
